1762fba41f764dba557a651ea4952845dd5aebf4
Olga Borodchak <olga.leonovna@gmail.com>
2026-06-22 15:32:10 -0400

    fix: logo link and education duplicates

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/Psychologist-Esther-Savina-V2',
+  base: '/Psychologist-Esther-Savina-V2/',
 
   resolve: {
     alias: {
